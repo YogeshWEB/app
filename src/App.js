a953d8147dc7d3758006f7bed6e932d1b1faa64c
YogeshWEB <neemaker246@gmail.com>
2022-09-07 20:41:26 +0530

    Revert "hardwork"
    This reverts commit 50fb2d16f54c24deaf4aa7eb1d718acdb6243b16.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,23 @@
-import React, {useEffect} from "react";
-import "./App.css";
-import Header from "./Header";
-import Login from "./Login";
-import {
-  BrowserRouter,
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
-import Checkout from "./Checkout"
-import Home from "./Home";
-import { auth } from "./firebase";
-import { useStateValue } from "./StateProvider";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
-  const [{}, dispatch] = useStateValue();
-  useEffect(() => {
-    // will only run once when the app component loads...
-
-    auth.onAuthStateChanged((authUser) => {
-      console.log("THE USER IS >>> ", authUser);
-
-      if (authUser) {
-        // the user just logged in / the user was logged in
-
-        dispatch({
-          type: "SET_USER",
-          user: authUser,
-        });
-      } else {
-        // the user is logged out
-        dispatch({
-          type: "SET_USER",
-          user: null,
-        });
-      }
-    });
-  }, []);
   return (
     <div className="App">
-      <Router>
-      <Header></Header>
-        <Routes>
-        <Route path="/login" element={<Login></Login>} />
-          <Route path="/checkout" element={<Checkout></Checkout>} /> 
-          <Route path="/" element={<Home></Home>} />
-        </Routes>
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
